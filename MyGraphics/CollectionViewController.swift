621320ec57399e8671abbe1438f5955f16d546da
LinShiwei <477623614@qq.com>
2016-01-23 23:20:52 +0800

    Add share function

--- MyGraphics/CollectionViewController.swift
+++ MyGraphics/CollectionViewController.swift
@@ -12,61 +12,82 @@ import Photos
 import ImageViewer
 
 class CollectionViewController: UICollectionViewController,CollectionViewWaterfallLayoutDelegate  {
-
+    //MARK: 自定义变量
     enum ProviderEditState {
         case Normal
-        case Delete
+        case Choose
     }
     var currentEditState = ProviderEditState.Normal
     let appFilePath = (NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0]) + "/"
     var myImageCollection = [NSManagedObject]()
     var settingObject : NSManagedObject?
     var imagePaths = [String]()
-    
-    
+    let isChoosing = false
+    //MARK: IBOutlet
     @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var chooseButton: UIBarButtonItem!
     @IBOutlet weak var deleteButton: UIBarButtonItem!
-    
-    @IBAction func cellCloseButton(sender: UIButton) {
-        let cell = (sender.superview)!.superview as! CollectionViewCell
-        let indexPath = self.collectionView!.indexPathForCell(cell)
-        
-        let managedContext = getManagedContext()
-        managedContext.deleteObject(myImageCollection[indexPath!.row])
-        do {
-            try managedContext.save()
-        }
-        catch let error as NSError {
-            print("Could not save \(error), \(error.userInfo)")
-        }
-        
-        self.myImageCollection.removeAtIndex(indexPath!.row)
-        self.collectionView!.performBatchUpdates({
-            self.collectionView!.deleteItemsAtIndexPaths([indexPath!])
-            return
-            }, completion: nil)
-    }
-    @IBAction func chooseImageToDelete(sender: UIBarButtonItem) {
-        if (self.currentEditState == ProviderEditState.Normal)
-        {
-            self.deleteButton.image = UIImage(named: "checkMark")
-            self.currentEditState = ProviderEditState.Delete;
-            
+    //MARK: IBAction
+    @IBAction func chooseButton(sender: UIBarButtonItem) {
+        if self.currentEditState == ProviderEditState.Normal{
+            self.currentEditState = ProviderEditState.Choose
+            chooseButton.title = "完成"
+            shareButton.enabled = true
+            deleteButton.enabled = true
+            self.collectionView!.allowsSelection = true
+            self.collectionView!.allowsMultipleSelection = true
             for cell in self.collectionView!.visibleCells() as! [CollectionViewCell]
             {
-                cell.closeButton.hidden = false
                 cell.cellImage.userInteractionEnabled = false
             }
         }else{
-            self.deleteButton.image = UIImage(named: "delete")
             self.currentEditState = ProviderEditState.Normal
-            
+            chooseButton.title = "选择"
+            shareButton.enabled = false
+            deleteButton.enabled = false
+            self.collectionView!.allowsMultipleSelection = false
+            self.collectionView!.allowsSelection = false
             for cell in self.collectionView!.visibleCells() as! [CollectionViewCell]
             {
-                cell.closeButton.hidden = true
                 cell.cellImage.userInteractionEnabled = true
             }
         }
+    }
+    
+    @IBAction func chooseImageToDelete(sender: UIBarButtonItem) {
+        var indexPath:[NSIndexPath] = self.collectionView!.indexPathsForSelectedItems()!
+        if indexPath.count > 0 {
+            if indexPath.count > 1{
+                //冒泡排序
+                for j in 1...indexPath.count - 1 {
+                    for i in 0...indexPath.count - j - 1{
+                        if indexPath[i].row < indexPath[i + 1].row {
+                        let temp = indexPath[i]
+                        indexPath[i] = indexPath[i + 1]
+                        indexPath[i + 1] = temp
+                        }
+                    }
+                    
+                }
+            }
+            let managedContext = getManagedContext()
+            for path in indexPath {
+                managedContext.deleteObject(myImageCollection[path.row])
+                self.myImageCollection.removeAtIndex(path.row)
+            }
+            do {
+                    try managedContext.save()
+                }
+                catch let error as NSError {
+                    print("Could not save \(error), \(error.userInfo)")
+                }
+            self.collectionView!.performBatchUpdates({
+                self.collectionView!.deleteItemsAtIndexPaths(indexPath)
+                return
+                }, completion: nil)
+        }
+        
+    
 
     }
     @IBAction func saveToPhotoAlbum(sender: UIBarButtonItem) {
@@ -86,27 +107,7 @@ class CollectionViewController: UICollectionViewController,CollectionViewWaterfa
 
         }
     }
-    func saveNextImage(){
-        if imagePaths.count > 0 {
-            let image = getImage(imagePaths.last!)
-             UIImageWriteToSavedPhotosAlbum(image, self, "imageSavedToPhotosAlbum:didFinishSavingWithError:contextInfo:", nil)
-            imagePaths.removeLast()
-        }
-        
-    }
-    func imageSavedToPhotosAlbum(image: UIImage, didFinishSavingWithError error: NSError?, contextInfo:UnsafePointer<Void>) {
-        if imagePaths.count > 0 {
-            saveNextImage()
-        } else {
-            if imagePaths.count == 0 {
-                let alert = UIAlertController(title: "Success!", message: "Photos have been saved to Album", preferredStyle: .Alert)
-                
-                let okAction = UIAlertAction(title: "OK", style: .Default) { (action: UIAlertAction) -> Void in }
-                alert.addAction(okAction)
-                presentViewController(alert, animated: true, completion: nil)
-            }
-        }
-    }
+    
     @IBAction func addImage(sender: AnyObject) {
         let hour = settingObject!.valueForKey("updateCycle") as! Double
         let date = NSDate(timeInterval: -3600*hour, sinceDate: NSDate())
@@ -128,7 +129,7 @@ class CollectionViewController: UICollectionViewController,CollectionViewWaterfa
                 imageManager.requestImageForAsset(imageAssets, targetSize: imageSize, contentMode: .AspectFill, options: imageRequestOptions, resultHandler: {image,_ in
                     let name = imageAssets.valueForKey("filename")!
                     let path = self.appFilePath + (name as! String)
-                    let data = UIImageJPEGRepresentation(image!, 1)
+                    let data = UIImageJPEGRepresentation(image!,1)
                     data!.writeToFile(path, atomically: true)
                     let managedContext = self.getManagedContext()
                     let entity = NSEntityDescription.entityForName("Image", inManagedObjectContext:managedContext)
@@ -147,43 +148,49 @@ class CollectionViewController: UICollectionViewController,CollectionViewWaterfa
             self.collectionView?.reloadData()
         }
     }
+    //MARK: view
     override func viewDidLoad() {
         super.viewDidLoad()
         settingObject = initsettingObject()
         myImageCollection = fetchImageObject()
+        shareButton.enabled = false
+        deleteButton.enabled = false
         self.collectionView!.collectionViewLayout = getLayout()
-        
         self.collectionView!.backgroundColor = UIColor(white: 0.95, alpha: 1)
     }
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        shareButton.enabled = false
-        
+//        shareButton.enabled = false
+//        deleteButton.enabled = false
+//        currentEditState = ProviderEditState.Normal
         if settingObject!.hasChanges {
             saveToCoreData()
             self.collectionView!.collectionViewLayout = getLayout()
             self.collectionView!.reloadData()
         }
     }
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+//        currentEditState = ProviderEditState.Normal
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-/*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
+        // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-       
-        if segue.identifier == "setPassword" {
-           
+        if segue.identifier == "share" {
+            print("segue")
+            let controller = segue.destinationViewController as! ShareListTableViewController
+            controller.indexPath = (self.collectionView!.indexPathsForSelectedItems())!
+            controller.imageManagedObject = myImageCollection
         }
-        
+       
     }
-    */
 
     // MARK: UICollectionViewDataSource
 
@@ -203,31 +210,32 @@ class CollectionViewController: UICollectionViewController,CollectionViewWaterfa
         // Configure the cell
         let imageURL = getImageURL(myImageCollection, indexPathRow: indexPath.row)
         
+        cell.selectedBackgroundView = UIView.init()
+        cell.selectedBackgroundView?.backgroundColor = UIColor(red: 0/255, green: 128/255, blue: 255/255, alpha: 1)
+        
         cell.cellImage.setupForImageViewer(NSURL(string: imageURL)!, backgroundColor: UIColor.blackColor())
         cell.cellImage.image = getImage(imageURL)
-        cell.closeButton.tag = indexPath.row
 //        cellPaths[indexPath.row] =indexPath
         if(self.currentEditState == ProviderEditState.Normal)
         {
-            cell.closeButton.hidden = true
             cell.cellImage.userInteractionEnabled = true
 
         }
         else
         {
-            cell.closeButton.hidden = false
             cell.cellImage.userInteractionEnabled = false
 
         }
-        let longPress = UILongPressGestureRecognizer(target: self, action: "longPressToShare")
-////        longPress.delegate = self
-        cell.cellImage.addGestureRecognizer(longPress)
+//        let longPress = UILongPressGestureRecognizer(target: self, action: "longPressToShare")
+//////        longPress.delegate = self
+//        cell.cellImage.addGestureRecognizer(longPress)
         return cell
     }
     override func collectionView(collectionView: UICollectionView, willDisplayCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: NSIndexPath) {
             cell.backgroundColor = UIColor(white: 0.95, alpha: 1)
         
     }
+    
     // MARK: UICollectionViewDelegate
 
     /*
@@ -258,23 +266,7 @@ class CollectionViewController: UICollectionViewController,CollectionViewWaterfa
     
     }
     */
-    
-//    func fetchUpdateCycle()->Double {
-//        var cycle:Double = 1.0
-//        let managedContext = getManagedContext()
-//        let fetchRequest = NSFetchRequest(entityName: "Setting")
-//        do {
-//            let object = try managedContext.executeFetchRequest(fetchRequest) as! [NSManagedObject]
-//            if object.count > 0{
-//                cycle = object[0].valueForKey("updateCycle") as! Double
-//            }
-//        } catch let error as NSError {
-//            print("Could not fetch \(error), \(error.userInfo)")
-//        }
-//        
-//        return cycle
-//    }
-    
+
     // MARK: WaterfallLayoutDelegate
     
     func collectionView(collectionView: UICollectionView, layout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
@@ -284,7 +276,7 @@ class CollectionViewController: UICollectionViewController,CollectionViewWaterfa
     func cellSize(imageObj:[NSManagedObject],indexPathRow:Int)->CGSize{
         let imageURL = getImageURL(myImageCollection, indexPathRow: indexPathRow)
         let image = getImage(imageURL)
-        let size = CGSize(width: image.size.width+9, height: image.size.height+9)
+        let size = CGSize(width: image.size.width+4, height: image.size.height+4)
         return size
     }
     func getLayout()->CollectionViewWaterfallLayout{
@@ -364,6 +356,27 @@ class CollectionViewController: UICollectionViewController,CollectionViewWaterfa
         shareTableViewController.popoverPresentationController?.sourceView = self.collectionView
         presentViewController(shareTableViewController,animated: true, completion:nil)
         
+    }
+    func saveNextImage(){
+        if imagePaths.count > 0 {
+            let image = getImage(imagePaths.last!)
+            UIImageWriteToSavedPhotosAlbum(image, self, "imageSavedToPhotosAlbum:didFinishSavingWithError:contextInfo:", nil)
+            imagePaths.removeLast()
+        }
+        
+    }
+    func imageSavedToPhotosAlbum(image: UIImage, didFinishSavingWithError error: NSError?, contextInfo:UnsafePointer<Void>) {
+        if imagePaths.count > 0 {
+            saveNextImage()
+        } else {
+            if imagePaths.count == 0 {
+                let alert = UIAlertController(title: "Success!", message: "Photos have been saved to Album", preferredStyle: .Alert)
+                
+                let okAction = UIAlertAction(title: "OK", style: .Default) { (action: UIAlertAction) -> Void in }
+                alert.addAction(okAction)
+                presentViewController(alert, animated: true, completion: nil)
+            }
+        }
     }
 }
 
